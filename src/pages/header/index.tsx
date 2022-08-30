e3dcf396564aef5ec81fb0728613e7da5a8c62f5
Gary Foster <gary.work.pro@gmail.com>
2022-08-29 22:05:12 -0400

    complete header

---
+++ src/pages/header/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "../../components";
+import styles from "./index.module.css";
+
+interface IHeaderProps {
+  onGetNewJoke: () => void;
+}
+
+const Header: React.FC<IHeaderProps> = ({ onGetNewJoke }) => {
+  return (
+    <div className={styles.header}>
+      <Button button_style="success" onClick={onGetNewJoke}>
+        Get A New Random Joke
+      </Button>
+      <a href="https://github.com/15Dkatz/official_joke_api" target="_blank">
+        View API Docs
+      </a>
+    </div>
+  );
+};
+
+export default Header;
